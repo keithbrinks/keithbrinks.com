4545363ae38c1d5274e891505d1750012fb9159f
Keith Brinks <keith@brinks.me>
2022-04-09 09:39:57 -0400

    Generate site

---
+++ dist/_nuxt/2998172.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{231:function(t,e,n){"use strict";n.r(e);var l=n(27),component=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("footer",{staticClass:"text-slate-400 mt-12 border-t pt-6 text-sm"})}),[],!1,null,null,null);e.default=component.exports;installComponents(component,{Footer:n(231).default})}}]);
